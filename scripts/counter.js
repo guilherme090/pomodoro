7c8ac9086e0645e37e4eafbf409f669405444ac9
Guilherme Simoes <guilherme-elec12@hotmail.com>
2023-02-08 22:50:49 -0300

    <feat> format date and time

--- scripts/counter.js
+++ scripts/counter.js
@@ -3,9 +3,11 @@ let newTime = 0;
 let theTimer = null;
 
 $(function(){
-    $("#timer").text("0.000");
+    $("#timer").text("00:00:00.000");
     $("#start-btn").on("click", timerClick);
     $("#reset-btn").on("click", resetTimer);
+    $("#start-time").text("");
+    $("#end-time").text("");
 });
 
 function timerClick() {
@@ -21,12 +23,16 @@ function startTimer() {
     startTime = Date.now();
     theTimer = setInterval(function() {
         newTime = (Date.now() - startTime) / 1000 + alreadyComputedTime;
-        $("#timer").text(newTime.toFixed(3));
+        showTime(newTime);
     }, 50);
+    if($("#start-time").text() === "") {
+        $("#start-time").text(new Date());
+    } 
     $("#start-btn").prop("value", "Stop Counter");
 }
 
 function stopTimer() {
+    $("#end-time").text(new Date()); 
     $("#start-btn").prop("value", "Start Counter");
     clearInterval(theTimer);
     theTimer = null;
@@ -36,5 +42,33 @@ function resetTimer() {
     stopTimer(theTimer);
     startTime = 0;
     newTime = 0;
-    $("#timer").text(startTime.toFixed(3));
+    showTime(startTime);
+    $("#start-time").text("");
+    $("#end-time").text("");
+}
+
+function showTime(time) {
+    let integralPart = Math.floor(time);
+    let seconds = time % 60;
+    let minutes = Math.floor(integralPart / 60) % 60; 
+    let hours = Math.floor(integralPart / 3600);
+
+    seconds = seconds.toFixed(3);
+    seconds = pad(seconds);
+    minutes = minutes.toFixed(0);
+    minutes = pad(minutes);
+    hours = hours.toFixed(0);
+    hours = pad(hours);
+
+    $("#timer").text(`${hours}:${minutes}:${seconds}`);
+}
+
+/*
+    Add leading zeros to format numbers that are lower than 10
+*/
+function pad (number) {
+    if(number < 10) {
+        return "0" + number;
+    }
+    return number;
 }
